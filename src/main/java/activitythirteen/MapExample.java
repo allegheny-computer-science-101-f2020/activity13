acafd9368077729b6867e136488f19288d4469d7
Janyl Jumadinova <jjumadinova@allegheny.edu>
2020-11-13 14:38:27 -0500

    Activity 13 files

---
+++ src/main/java/activitythirteen/MapExample.java
@@ -0,0 +1,16 @@
+package activitythirteen;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/** A program that demonstrate the use of Map, printing the the key value pairs. */
+public class MapExample {
+
+  /** The main method for the MapExample program. */
+  public static void main(String[] args) {
+    TreeMap<Integer, String> map = new TreeMap<>();
+
+    System.out.println("Adding values to the map.\n");
+  }
+}
